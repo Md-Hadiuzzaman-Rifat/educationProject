dc47a17a99cfb1ce4cfbc4151d79baff24327a93
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-04-30 20:14:53 +0600

    design done

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/quiz" element={<Quiz />}></Route>
+        <Route path="/result" element={<Result />}></Route>
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
